b693ff1145c58c8caf0bbcd3713cfba995d7ed64
srinu391 <srinukummari123@gmail.com>
2024-12-08 16:40:12 +0530

    Update css.

--- src/App.js
+++ src/App.js
@@ -6,36 +6,38 @@ import './App.css';
 function App() {
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
+  const [error, setError] = useState(false);
 
   const handleCalculate = () => {
     try {
       const calculationResult = stringCalculator(input);
       setResult(`Result: ${calculationResult}`);
+      setError(false)
     } catch (error) {
       setResult(`Error: ${error.message}`);
+      setError(true);
     }
   };
 
   return (
-    <div className="App">
-      <h1>String Calculator</h1>
-      <div>
-        <label htmlFor="inputString">Enter numbers:</label>
+    <div className="container">
+      <h1 className="title">String Calculator</h1>
+      <div className="inputContainer">
         <input
           type="text"
           id="inputString"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="e.g., 1,2,3"
-          style={{ margin: "10px", padding: "5px" }}
         />
-      </div>
-      <button onClick={handleCalculate} style={{ padding: "5px 10px", cursor: "pointer" }}>
+        <button onClick={handleCalculate}>
         Calculate
       </button>
-      <p style={{ marginTop: "10px", fontWeight: "bold" }}>{result}</p>
+      </div>
+      <p className={error ? 'result error': 'result'}>{result}</p>
     </div>
   );
 }
 
 export default App;
+
